ebaf015620f6cdbe8fcac99ba7250c64e344ffed
DuneDune29 <rjarorgudwns@naver.com>
2024-08-07 17:55:29 +0900

    commit

--- src/Array/Test12.java
+++ src/Array/Test12.java
@@ -5,17 +5,31 @@ public class Test12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int size1 = scanner.nextInt();
-        int size2 = scanner.nextInt();
+        int student = scanner.nextInt();
+        int test = scanner.nextInt();
 
-        int[][] arr = new int[size1][size2];
+        int[][] arr = new int[test][student];
 
-        for (int i = 0; i < size1; i++) {
-            for (int j = 0; j < size2; j++) {
+        for (int i = 0; i < test; i++) {
+            for (int j = 0; j < student; j++) {
                 arr[i][j] = scanner.nextInt();
-
             }
         }
+
+        for(int i = 1; i <= student; i++) {
+            for (int j = 1; j <= student; j++) {
+                int n = 0;
+                for (int k = 0; k < test; k++) {
+                    int mentor = 0;
+                    int mentee = 0;
+                    for (int l = 0; l < student; l++) {
+                        if (arr[k][l] == i) mentor = l;
+                        if (arr[k][l] == j) mentee = l;
+                    }
+                }
+            }
+        }
+
         System.out.println();
     }
 }
